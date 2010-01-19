9a9339d1297d808fbb1885666d6883e586c924a8
Jani Monoses <jani@ubuntu.com>
2010-01-19 23:37:11 +0200

    add ro text, meps, subj

---
+++ ro.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+meps = [
+		"Mr jani.monoses@gmail.com",
+		]
+mepsi = [
+	"Mr adrian.severin@europarl.europa.eu",
+	"Mr cristiandan.preda@europarl.europa.eu",
+	"Mr csaba.sogor@europarl.europa.eu",
+	"Mr george.becali@europarl.europa.eu",
+	"Mr georgesabin.cutas@europarl.europa.eu",
+	"Mr ioanmircea.pascu@europarl.europa.eu",
+	"Mr iosif.matula@europarl.europa.eu",
+	"Mr iuliu.winkler@europarl.europa.eu",
+	"Mr marian-jean.marinescu@europarl.europa.eu",
+	"Mr theodordumitru.stolojan@europarl.europa.eu",
+	"Mr traian.ungureanu@europarl.europa.eu",
+	"Mr tudorcorneliu.vadim@europarl.europa.eu",
+	"Mr victor.bostinaru@europarl.europa.eu",
+	"Mr catalin-sorin.ivan@europarl.europa.eu",
+	"Ms claudiuciprian.tanasescu@europarl.europa.eu",
+	"Ms elenaoana.antonescu@europarl.europa.eu",
+	"Ms norica.nicolai@europarl.europa.eu",
+	"Ms renate.weber@europarl.europa.eu",
+	"Ms rovana.plumb@europarl.europa.eu",
+	"Ms vasilicaviorica.dancila@europarl.europa.eu"
+	]
+
+mail_subj = "Vă rog să semnaţi Declaraţia Scrisă 54/2009"
+
+mail_body = """
+
+Vă rog să semnaţi Declaraţia Scrisă 54/2009 - avem nevoie de susţinerea Dumneavoastră pentru a pune capăt celui mai grav caz de abuz asupra cailor din Europa
+
+Vă scriu pentru a vă cere ajutorul – vrem să punem capăt suferinţei a sute de mii de cai care an de an sunt transportaţi mii de kilometri în Europa pentru a fi abatorizaţi.
+În fiecare an, în Europa, 100.000 de cai îndură condiţii cumplite, suportând călătorii care durează zile în şir, fără odihnă, hrană sau apă, în arşiţa verii sau în îngheţul iernii. Legislaţia în vigoare, Reglementarea Consiliului European (CE) 1/2005, suferă de lacune serioase, iar unele dintre statele membre nu o aplică la modul riguros - pentru aceşti cai acest lucru înseamnă epuizare, vătămare, durere şi stres.
+Aceste călătorii nu sunt doar inumane, ele sunt şi cu totul inutile.
+Cercetările World Horse Welfare demonstrează că există situaţii în care caii transportaţi spre abatorizare trec în drumul lor pe lângă 180 de abatoare certificate înainte să ajungă la destinaţie. Considerăm acest lucru anormal. În loc să fie supuşi unor zile inutile de suferinţă, caii ar trebui sacrificaţi cât mai aproape posibil de punctul de origine şi apoi transportate carcasele.
+În acest scop a fost iniţiată Declaraţia Scrisă (54/2009), prin care li se cere Membrilor Parlamentului European să susţină revizuirea legislaţie europene cu privire la transportul animalelor şi în special al cailor. Prin urmare, vă rog să vă arătaţi susţinerea pentru Declaraţia 54/2009 şi să o semnaţi înainte de 21 ianuarie 2010. Vă rog de asemenea să îi încurajaţi şi pe colegii Dvs. din Parlamentul European să facă acelaşi lucru.
+132,565 de cetăţeni Europeni au semnat o petiţie prin care cer să se pună capăt acestei practici barbare, petiţie care a fost de curând înmânată Comisiei Europe. Alte mii de persoane îi trimit cărţi poştale D-lui Barroso, cerând acelaşi lucru. Avem acum nevoie de ajutorul Dvs. pentru a pune capăt odată şi pentru totdeauna acestei suferinţe.
+Dacă doriţi şi alte informaţii legate de transportul cailor pe distanţe lungi pentru abatorizare, vă rugăm să luaţi legătura cu World Horse Welfare, la +44 (0)1953 497226 (e:hannahlynch@worldhorsewelfare.org) sau intraţi pe site-ul www.worldhorsewelfare.org.
+Cu respect,
+"""
+
