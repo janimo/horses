f48c3a5219f9b0456b8a5546e9dafb03f2abd8a1
Jani Monoses <jani@tradesense.ro>
2010-02-08 18:02:32 +0200

    generate python dictionary from xls

--- tmp/getmeps.py
+++ tmp/getmeps.py
@@ -8,16 +8,19 @@ def do_one_sheet(s):
     for r in range(1,s.nrows):
         row = s.row_values(r)
         if row[1] == '':
-            print(row[0], row[2])
+            print('"'+row[2].strip()+'",')
 
 def get_meps(file):
     wb = xlrd.open_workbook(file)
     for sn in wb.sheet_names():
         s = wb.sheet_by_name(sn)
-        print(sn)
+        print('"'+sn+'": [')
         do_one_sheet(s)
+        print("],")
 
 if __name__ == "__main__":
+    print("meps = {")
     get_meps(xlsfile)
+    print("}")
 
 # vim: set ts=4 sw=4 expandtab :
